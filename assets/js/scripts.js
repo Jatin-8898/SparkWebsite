234704a1996d49b8c7bf5d90641e3b614381beb9
Jatin Varlyani <varlyanijatin88@gmail.com>
2018-08-22 19:27:18 +0530

    First Commit

---
+++ assets/js/scripts.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){   
+
+    /************************TYPED JS**********************************/
+
+    $('.slider').ripples({
+        resolution: 1024,
+	    dropRadius: 10,
+	    perturbance: 0.01,
+        crossOrigin: null,
+    });
+    
+    /************************TYPED JS**********************************/
+    
+    var typed = new Typed('#typed', {
+        //stringsElement: '#typed-strings'
+        strings: ['We are new in <strong class="primary">JavaScript...</strong>', 'We are  becoming<strong class="secondary">Awesome Developers</strong>'],
+        typeSpeed: 50,
+        backDelay: 1000,
+        backSpeed: 10,
+        fadeOut: false,
+        fadeOutClass: 'typed-fade-out',
+        fadeOutDelay: 1500,
+        showCursor: true,
+        cursorChar: '|',
+        autoInsertCss: true,
+        loop: true,
+    });
+    
+    
+    
+    /************************TYPED JS**********************************/
+    
+    $(window).scroll(function(){
+       var top = $(window).scrollTop();
+        //console.log(top);
+        if(top > 170){
+            /*When it goes after the hero area add the color to the navbar*/
+            $('header').addClass('transparent-bg');
+            
+        }else{
+            /*When the user is in hero area that time hide the color by removing the transparent-bg class if it has*/
+            if($('header').hasClass('transparent-bg')){
+                $('header').removeClass('transparent-bg');
+            }
+            
+        }
+        
+    });
+    
+});
